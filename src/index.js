50b4b8452d3b13552f9e06474d31ede31b0c403b
Ujjwal Kaushik <166251292+Ujjwal250802@users.noreply.github.com>
2025-07-23 12:26:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chatapplicationfrontend-smoky.vercel.app",
     credentials: true, // allow frontend to send cookies
   })
 );
